7756db70a6cd636198947532790a345d55876c11
NicoGER1 <nico3g@hotmail.fr>
2024-06-06 21:33:51 +0200

    fixe the responsive of the background image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
-export function Home() {
+import Map from "../components/Map";
 
+function Home() {
   return (
     <section className="HomeComponent">
-      <h1> coucou</h1>
       <Map />
     </section>
   );
