6b591659dda048a04cc6df6998e7f8b847aa0d2c
NicoGER1 <nico3g@hotmail.fr>
2024-06-06 22:13:09 +0200

    update map position

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,8 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <main className="container">
-        <section className="text-box">
-          <h1 className="block-primary">
-            <span className="block-primary-main">Street Art Hunter</span>
-          </h1>
-        </section>
-      </main>
-      <section>
-        <Outlet />
-      </section>
+      <Outlet />
+
       <NavBar />
     </>
   );
