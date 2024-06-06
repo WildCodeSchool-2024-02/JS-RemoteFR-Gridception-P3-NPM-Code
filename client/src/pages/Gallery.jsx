b1673b1e4467a07dce912a4ba068cc99d90edfd6
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-06-06 15:45:57 +0200

    Created Gallery Page

---
+++ client/src/pages/Gallery.jsx
@@ -0,0 +1,11 @@
+function Gallery() {
+  return (
+    <section className="GalleryComponent">
+      <h1>Gallery</h1>
+
+      <p>Gallery Component</p>
+    </section>
+  );
+}
+
+export default Gallery;
