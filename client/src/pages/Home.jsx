194bb5ab80529de09b0034c552fd8b8b8802a22e
NicoGER1 <nico3g@hotmail.fr>
2024-06-06 17:48:06 +0200

    map component ok

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,8 @@
-import Map from '../components/Map'
-
 function Home() {
   return (
     <section className="HomeComponent">
       <h1>Home</h1>
-      <Map />
+
       <p>Home Component</p>
     </section>
   );
