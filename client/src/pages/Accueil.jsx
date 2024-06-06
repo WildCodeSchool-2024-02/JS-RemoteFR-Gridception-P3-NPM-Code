22925d09afd4a5614c58706ec4878750e060f34c
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-06-06 15:38:37 +0200

    Created Accueil Page

---
+++ client/src/pages/Accueil.jsx
@@ -0,0 +1,11 @@
+function Accueil() {
+  return (
+    <section className="AccueilComponent">
+      <h1>Accueil</h1>
+
+      <p>Accueil Component</p>
+    </section>
+  );
+}
+
+export default Accueil;
