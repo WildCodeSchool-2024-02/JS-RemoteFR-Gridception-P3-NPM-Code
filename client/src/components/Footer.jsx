e44b1b99118efb20d2dbe4a3905eeff356dd53d1
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-06-11 17:17:11 +0200

    Page Home soon finish

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,21 +8,13 @@ function Footer() {
       <li>
         <NavLink to="/mentions_legales">Conditions d'utilisation</NavLink>
       </li>
-      <li>
-        <NavLink to="/cgu">CGU</NavLink>
-      </li>
-      <li>
-        <a
-          href="https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-Gridception-P3-NPM-Code"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={GithubIcon} alt="Github icon" className="github-icon" />
-        </a>
-      </li>
-      <li>
-        <NavLink to="/cgu">Notre équipe</NavLink>
-      </li>
+      <a
+        href="https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-Gridception-P3-NPM-Code"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={GithubIcon} alt="Github icon" className="github-icon" />
+      </a>
       <li>© Copyright NPM Code</li>
     </nav>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,23 @@
+import { Link } from "react-router-dom";
 import Map from "../components/Map";
+
+import Addpicture from "../assets/images/addpicture.png";
 
 function Home() {
   return (
     <section className="main-desktop">
       <article className="text-content">
-        <h1 className="main-title">Street art Hunter</h1>
+        <h1 className="main-title">
+          STREET ART <br /> HUNTER
+        </h1>
         <h2 className="second-title">La chasse commence ici</h2>
         <div className="buttons">
           <button type="button" className="button-connect">
-            Connexion
+            Se connecter
           </button>
-          <button type="button" className="button-add">
-            +
-          </button>
+          <Link className="button-add" to="/add" activeClassName="active">
+            <img src={Addpicture} alt="Icone pour ajouter une oeuvre" />
+          </Link>
         </div>
       </article>
       <Map />
