501aacfec715abe1b99884cfad7b0daec2913535
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-06-11 14:58:42 +0200

    Fix the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,23 @@
 import { NavLink } from "react-router-dom";
 
+import GithubIcon from "../assets/images/github.png";
+
 function Footer() {
   return (
     <nav className="footer">
       <li>
         <NavLink to="Legalnotice">Notice</NavLink>
       </li>
-      <li>Github</li>
-      <li>Copyright NPM Code</li>
+      <li>
+        <a
+          href="https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-Gridception-P3-NPM-Code"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={GithubIcon} alt="Github icon" className="github-icon" />
+        </a>
+      </li>
+      <li>© Copyright NPM Code</li>
     </nav>
   );
 }
