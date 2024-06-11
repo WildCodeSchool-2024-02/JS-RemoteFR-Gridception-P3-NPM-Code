825483137e45323b8b542e9fadaeeb67ae3af221
pierre delarocque <delarocque.pierre@gmail.com>
2024-06-11 12:35:59 +0200

    creation dossier js

---
+++ client/src/js/oeuvres.js
@@ -0,0 +1,25 @@
+import japonaise from "../assets/images/imageOeuvres/soleillevant.jpg";
+import japonaise2 from "../assets/images/imageOeuvres/japonaiseparasol.jpg";
+
+const oeuvres = [
+  {
+    name: "Japonaise dénudée",
+    image: japonaise,
+    longitude: "47°23'47.9",
+    latitude: "0°40'49.8",
+    description:
+      " la femme est légèrement dénudée. Elle laisse glisser son kimono, imprimé de signes rouges, faisant apparaître son épaule droite. Le regard vers le bas, les cheveux noirs noués et le teint blanc. Elle est synonyme de sensualité. Sous les marronniers du bord de Loire, elle colore un paravent en tôle qui cache les toilettes des hommes.",
+    localitée: "Tours",
+  },
+  {
+    name: "Japonaise avec un baton",
+    image: japonaise2,
+    longitude: "47°23'43.0",
+    latitude: "0°41'07.9",
+    description:
+      "tient une sorte de bâton noir. Elle regarde à droite. Sa tête est englobée par un soleil flamboyant.",
+    localitée: "Tours",
+  },
+];
+
+export default oeuvres;
