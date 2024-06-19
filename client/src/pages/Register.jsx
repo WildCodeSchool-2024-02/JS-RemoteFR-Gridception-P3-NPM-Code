d1930862f6d015718536b2d7cae1600f252076c6
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-06-19 18:48:33 +0200

    Start register functional

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 import logo from "../assets/images/logo.png";
 import Tetris from "../assets/images/Tetris.gif";
 
 function Register() {
+  const [registerForm, setRegisterForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleRegisterChange = (event) => {
+    const { name, value } = event.target;
+    setRegisterForm({ ...registerForm, [name]: value });
+  };
+
+  const notifySuccess = () =>
+    toast.success("Inscription réussie !", {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
+  const notifyError = () =>
+    toast.error(
+      "Il nous manque une information, vérifiez que vous avez rempli tous les champs",
+      {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      }
+    );
+
   return (
     <section className="form-container">
       <img src={logo} alt="Street Art Hunter logo" className="logoMobile" />
@@ -22,19 +65,51 @@ function Register() {
         <div className="form-left">
           <div className="form-group">
             <label htmlFor="firstName">Prénom</label>
-            <input required name="firstName" id="firstName" type="text" />
+            <input
+              required
+              name="firstName"
+              id="firstName"
+              type="text"
+              minLength={3}
+              maxLength={50}
+              onChange={handleRegisterChange}
+              value={registerForm.firstName}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="lastName">Nom</label>
-            <input required name="lastName" id="lastName" type="text" />
+            <input
+              required
+              name="lastName"
+              id="lastName"
+              type="text"
+              minLength={3}
+              maxLength={70}
+              onChange={handleRegisterChange}
+              value={registerForm.lastName}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="email">Adresse E-Mail</label>
-            <input required name="email" id="email" type="email" />
+            <input
+              required
+              name="email"
+              id="email"
+              type="email"
+              onChange={handleRegisterChange}
+              value={registerForm.email}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Mot de passe</label>
-            <input required name="password" id="password" type="password" />
+            <input
+              required
+              name="password"
+              id="password"
+              type="password"
+              onChange={handleRegisterChange}
+              value={registerForm.password}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="confirmPassword">Confirmez le mot de passe</label>
@@ -43,9 +118,53 @@ function Register() {
               name="confirmPassword"
               id="confirmPassword"
               type="password"
+              onChange={handleRegisterChange}
+              value={registerForm.confirmPassword}
             />
           </div>
-          <button type="submit" className="form-submit-btn">
+          <button
+            type="submit"
+            className="form-submit-btn"
+            onClick={(e) => {
+              e.preventDefault();
+              if (
+                registerForm.title !== "" &&
+                registerForm.description !== "" &&
+                registerForm.longitude !== "" &&
+                registerForm.latitude !== ""
+              ) {
+                axios
+                  .post(
+                    `${import.meta.env.VITE_API_URL}/api/street_arts`,
+                    registerForm
+                  )
+                  .then((res) => {
+                    notifySuccess();
+                    setRegisterForm({
+                      users_id: "9",
+                      title: "",
+                      description: "",
+                      artist: "",
+                      latitude: "",
+                      longitude: "",
+                      is_valid: 1,
+                    });
+                    console.info(res);
+                  })
+                  .catch((err) => {
+                    console.info(err);
+                  });
+              } else if (
+                registerForm.file === "" ||
+                registerForm.title === "" ||
+                registerForm.description === "" ||
+                registerForm.longitude === "" ||
+                registerForm.latitude === ""
+              ) {
+                notifyError();
+              }
+            }}
+          >
             S'enregistrer
           </button>
         </div>
@@ -56,6 +175,7 @@ function Register() {
           <img src={Tetris} alt="Preview" />
         </div>
       </form>
+      <ToastContainer />
     </section>
   );
 }
