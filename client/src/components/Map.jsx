39ad5c2bf95c0a5580da8e54adac2081744f27a8
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-06-27 22:20:05 +0200

    Hotfix the marker with beautiful buttons

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -3,8 +3,8 @@ import { useEffect, useRef, useState } from "react";
 
 import mapboxgl from "mapbox-gl";
 
-import Add from "../assets/images/add_icon2.png";
-import Info from "../assets/images/infos2.png";
+import Add from "../assets/images/add.png";
+import Info from "../assets/images/info.png";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
